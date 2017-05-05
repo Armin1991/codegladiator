3cc7688ec6c3b19971c819d52773e4c85440b299
Subhasish Panda <panda.subhasish007@gmail.com>
2017-05-05 17:28:06 +0530

    Change into code

--- backend/databaseCommunicator/handlerClass.py
+++ backend/databaseCommunicator/handlerClass.py
@@ -7,3 +7,6 @@ class List(Resource):
         cursor = mongo.listAll()
         list =[{m:str(x[m]) for m in x}  for x in cursor]
         return flask.jsonify({"total records":len(list),"data":list})
+class HelloWorld(Resource):
+    def get(self):
+        return flask.jsonify({"Success":"true","Message":"Hello World"})
--- backend/databaseCommunicator/main.py
+++ backend/databaseCommunicator/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from handlerClass import List
+from handlerClass import HelloWorld
 
 # import FMJ
 
@@ -12,13 +13,13 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 api = Api(app)
 api.add_resource(List,'/codegladiator/api/list')
-# api.add_resource(KeyTerms,'/termextractor/api/keyTerms')
+api.add_resource(HelloWorld,'/termextractor/api/hello')
 # api.add_resource(Scores,'/termextractor/api/scores')
 # api.add_resource(Phrases,'/termextractor/api/phrases')
 #api.add_resource(FMJ,'/termextractor/api/fmj')
 
 
-app.run(debug=True,host='127.0.0.1')
+app.run(debug=True,host='0.0.0.0')
 
 if __name__ == '__main__':
     app.run(debug=True)
