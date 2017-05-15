f5244473854ea4535391eda1e39e126811c2a918
Subhasish Panda <panda.subhasish007@gmail.com>
2017-05-15 12:43:01 +0530

    Commiting the logger file

--- backend/databaseCommunicator/logger.py
+++ backend/databaseCommunicator/logger.py
@@ -1,17 +1,17 @@
 import logging
 import datetime
 
+
 class Logger(object):
+
     def __init__(self):
         self.format = '%(asctime)-15s -INFO- %(message)s'
         self.date = datetime.datetime.now().strftime("%Y-%m-%d")
-        logging.basicConfig(filename ="HelloWorld"+ self.date+ '-error.log',filemode = 'a',level=logging.DEBUG, format = self.format)
-        #logging.info("Application Started")
+        logging.basicConfig(filename="HelloWorld" + self.date + '-error.log', filemode='a', level=logging.DEBUG, format=self.format)
         self.logger = logging.getLogger(__name__)
 
-    def message(self,string):
+    def message(self, string):
         self.logger.info(string)
 
-    def error(self,error):
+    def error(self, error):
         self.logger.error(error)
-
