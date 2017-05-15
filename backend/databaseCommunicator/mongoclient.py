e1a84f5b102c1d9dc9b67c1fec4219ef0245b186
root <panda.subhasish007@gmail.com>
2017-05-15 15:15:01 +0530

    Pre commit hook test

--- backend/databaseCommunicator/mongoclient.py
+++ backend/databaseCommunicator/mongoclient.py
@@ -1,31 +1,22 @@
-
 import pymongo
-
 from logger import Logger
 from requests import Requests
+
+
 class Mongo(object):
+
     def __init__(self):
         self.logger = Logger()
         self.request = Requests()
         self.hostPort = self.request.getHostPort("mongoDB")
-        self.client = pymongo.MongoClient(host=self.hostPort['Host'],port=self.hostPort["Port"])
+        self.client = pymongo.MongoClient(host=self.hostPort['Host'], port=self.hostPort["Port"])
         self.database = self.client.codeGladiator
         self.collection = self.database.testdata
 
-    def insert(self,json):
+    def insert(self, json):
         id = self.collection.insert_one(json)
         return id
+
     def listAll(self):
         cursor = self.collection.find()
         return cursor
-# data = pandas.read_excel("/home/subhasishp/Documents/codegladiator.xlsx")
-# rows = len(data.index)
-#
-# for row in range(rows):
-#     dict = {}
-#     for d in data:
-#         dict[d] = data[d][row]
-#
-#     id = mongo.insert(dict)
-#     print id
-
