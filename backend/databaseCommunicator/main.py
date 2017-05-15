f4dba4bd56c5462ca41be87c7827e3a245147ff1
Subhasish Panda <panda.subhasish007@gmail.com>
2017-05-15 14:46:15 +0530

    pre commit trial

--- backend/databaseCommunicator/main.py
+++ backend/databaseCommunicator/main.py
@@ -4,22 +4,14 @@ from handlerClass import List
 from handlerClass import HelloWorld
 from logger import Logger
 # import FMJ
-
-
 app = Flask(__name__)
-
-
 
 app.config['DEBUG'] = True
 api = Api(app)
-api.add_resource(List,'/codegladiator/api/list')
-api.add_resource(HelloWorld,'/codegladiator/api/hello')
-# api.add_resource(Scores,'/termextractor/api/scores')
-# api.add_resource(Phrases,'/termextractor/api/phrases')
-#api.add_resource(FMJ,'/termextractor/api/fmj')
-
+api.add_resource(List, '/codegladiator/api/list')
+api.add_resource(HelloWorld, '/codegladiator/api/hello')
 
 if __name__ == '__main__':
     log = Logger()
     log.message("Starting the application")
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
