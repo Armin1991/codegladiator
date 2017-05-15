f79295d90a4c81443d26536dc4cb58840d16db01
Subhasish Panda <panda.subhasish007@gmail.com>
2017-05-15 12:35:14 +0530

    change in handlerClass

--- backend/databaseCommunicator/handlerClass.py
+++ backend/databaseCommunicator/handlerClass.py
@@ -1,13 +1,18 @@
 import flask
-
 from mongoclient import Mongo
 from flask_restful import Resource
+
+
 class List(Resource):
+
     def get(self):
         mongo = Mongo()
         cursor = mongo.listAll()
-        list =[{m:str(x[m]) for m in x}  for x in cursor]
-        return flask.jsonify({"total records":len(list),"data":list})
+        list = [{m: str(x[m]) for m in x} for x in cursor]
+        return flask.jsonify({"total records": len(list), "data": list})
+
+
 class HelloWorld(Resource):
+
     def get(self):
-        return flask.jsonify({"Success":"true","Message":"Hello World"})
+        return flask.jsonify({"Success": "true", "Message": "Hello World"})
